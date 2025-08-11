37ff98b41ed827c8aed3a8403a1517c04c06c09b
nurazon59 <5shi.icyu@gmail.com>
2025-08-12 00:28:20 +0900

    neo treeの設定修正

--- config/.config/nvim/lua/plugins/neo-tree.lua
+++ config/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,6 +1,9 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
+    window = {
+      width = 25,  -- デフォルトは40なので、25に縮小
+    },
     filesystem = {
       filtered_items = {
         hide_dotfiles = false,
@@ -11,6 +14,32 @@ return {
           ".history",
         },
       },
+    },
+  },
+  keys = {
+    {
+      "<Space>e",
+      function()
+        local manager = require("neo-tree.sources.manager")
+        local renderer = require("neo-tree.ui.renderer")
+        
+        -- Neo-treeのウィンドウが開いているかチェック
+        local state = manager.get_state("filesystem")
+        if state and renderer.window_exists(state) then
+          -- 現在のウィンドウがNeo-treeかチェック
+          if vim.bo.filetype == "neo-tree" then
+            -- Neo-treeにいる場合は前のウィンドウ（エディタ）に戻る
+            vim.cmd("wincmd p")
+          else
+            -- エディタにいる場合はNeo-treeにフォーカス
+            vim.cmd("Neotree focus")
+          end
+        else
+          -- Neo-treeが開いていない場合は開く
+          vim.cmd("Neotree toggle")
+        end
+      end,
+      desc = "Toggle Neo-tree focus between tree and editor",
     },
   },
 }
