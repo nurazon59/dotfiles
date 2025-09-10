ad8a22428b12bfb2d9c1bb49bc1fa4088634091d
Itsuki Koshiishi <104209850+nurazon59@users.noreply.github.com>
2025-09-10 10:13:09 +0900

    git blame,markdownの修正 (#159)
    Co-authored-by: koshiishi_sansan <koshiishi_sansan@users.noreply.github.com>

--- config/.config/nvim/lua/config/autocmds.lua
+++ config/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- why: Markdown等でスペルチェックにより波線(undercurl)が大量表示され煩わしいため無効化する
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "markdown", "gitcommit", "text" },
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
