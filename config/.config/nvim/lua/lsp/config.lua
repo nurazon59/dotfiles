c69c0de7a8c4e90ba17927f3bbe0d1424e01b0ea
Itsuki Koshiishi <104209850+nurazon59@users.noreply.github.com>
2026-02-17 00:03:03 +0900

    nurazon59/fix/lspconfig (#407)
    - **fix: lspconfig**
    - **feat: add lspkind.nvim**
    
    ---------
    
    Co-authored-by: nurazon59 <nurazon59@users.noreply.github.com>

--- config/.config/nvim/lua/lsp/config.lua
+++ config/.config/nvim/lua/lsp/config.lua
@@ -1,6 +1,9 @@
 return {
+  -- LazyVimのblink.cmpを無効化（nvim-cmpに置き換え）
+  { "saghen/blink.cmp", enabled = false },
+
   {
-    "williamboman/mason.nvim",
+    "mason-org/mason.nvim",
     build = ":MasonUpdate",
     config = function()
       require("mason").setup({
@@ -9,10 +12,10 @@ return {
     end,
   },
   {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim" },
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = { "mason-org/mason.nvim" },
     config = function()
-      local setup = require("lsp.setup")
+      local setup = require("config.lsp-servers")
       require("mason-lspconfig").setup({
         ensure_installed = setup.servers,
         automatic_installation = true,
@@ -20,15 +23,95 @@ return {
     end,
   },
   {
-    "neovim/nvim-lspconfig",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "mason-org/mason.nvim" },
+    config = function()
+      require("mason-tool-installer").setup({
+        ensure_installed = require("config.lsp-servers").tools,
+      })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    enabled = true,
+    event = "InsertEnter",
     dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "hrsh7th/nvim-cmp",
       "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
       "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+      "onsails/lspkind.nvim",
     },
     config = function()
-      require("lsp.setup")
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      require("luasnip.loaders.from_vscode").lazy_load()
+
+      local lspkind = require("lspkind")
+
+      cmp.setup({
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        formatting = {
+          format = lspkind.cmp_format({
+            mode = "symbol_text",
+            maxwidth = 50,
+            ellipsis_char = "...",
+          }),
+        },
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+          { name = "path" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "mason-org/mason-lspconfig.nvim",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      require("config.lsp-servers").setup()
     end,
   },
 }
