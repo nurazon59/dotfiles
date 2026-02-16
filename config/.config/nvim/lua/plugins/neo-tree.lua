b3d59e9e2e2dd3f52840647cae170e5e50311253
Itsuki Koshiishi <104209850+nurazon59@users.noreply.github.com>
2026-02-17 00:02:47 +0900

    bump: plugin (#411)
    Co-authored-by: nurazon59 <nurazon59@users.noreply.github.com>

--- config/.config/nvim/lua/plugins/neo-tree.lua
+++ config/.config/nvim/lua/plugins/neo-tree.lua
@@ -1,5 +1,10 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
   opts = {
     window = {
       width = 30,
