0308bd8240a0345811b7d860c64825ab28ed7605
Itsuki Koshiishi <104209850+nurazon59@users.noreply.github.com>
2026-02-17 00:35:39 +0900

    nurazon59/feat/nvim/line ui (#409)
    - **fix: lspconfig**
    - **feat(nvim): add which-key**
    - **fix(nvim): lualine,bufferline ui**
    
    ---------
    
    Co-authored-by: nurazon59 <nurazon59@users.noreply.github.com>

---
+++ config/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,14 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_c = { "filename" },
+      lualine_x = { "lsp_status" },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
+    },
+  },
+}
