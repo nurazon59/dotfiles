b880798fefcc9f9d3c2c9fc6f538ca90ee94ccf9
nurazon59 <nurazon59@users.noreply.github.com>
2026-02-22 13:37:45 +0900

    feat(nvim): copilot lsp

---
+++ config/.config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,20 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  opts = {
+    suggestion = {
+      enabled = true,
+      auto_trigger = true,
+      keymap = {
+        accept = "<C-l>",
+        accept_word = "<M-k>",
+        accept_line = "<M-j>",
+        next = "<M-]>",
+        prev = "<M-[>",
+        dismiss = "<C-]>",
+      },
+    },
+    panel = { enabled = false },
+  },
+}
