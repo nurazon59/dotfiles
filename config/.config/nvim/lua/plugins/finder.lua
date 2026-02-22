b269b0f32f4720c8ac44df8a5d308acec6e73dc2
nurazon59 <nurazon59@users.noreply.github.com>
2026-02-23 02:04:50 +0900

    chore(nvim): bump finder

---
+++ config/.config/nvim/lua/plugins/finder.lua
@@ -0,0 +1,90 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      require("fzf-lua").setup({
+        "telescope",
+        winopts = {
+          height = 0.85,
+          width = 0.80,
+          row = 0.35,
+          col = 0.50,
+          preview = {
+            layout = "horizontal",
+            horizontal = "right:50%",
+          },
+        },
+        keymap = {
+          builtin = {
+            ["<C-d>"] = "preview-page-down",
+            ["<C-u>"] = "preview-page-up",
+          },
+        },
+        files = {
+          prompt = "Files❯ ",
+          cmd = "fd --type f --hidden --follow --exclude .git",
+        },
+        grep = {
+          prompt = "Rg❯ ",
+          cmd = "rg --vimgrep --hidden --glob '!.git'",
+        },
+      })
+
+      local opts = { noremap = true, silent = true }
+      vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<CR>", opts)
+      vim.keymap.set("n", "<leader>fg", "<cmd>FzfLua live_grep<CR>", opts)
+      vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<CR>", opts)
+      vim.keymap.set("n", "<leader>fc", "<cmd>FzfLua commands<CR>", opts)
+      vim.keymap.set("n", "<leader>fd", "<cmd>FzfLua diagnostics_document<CR>", opts)
+      vim.keymap.set("n", "<leader>fD", "<cmd>FzfLua diagnostics_workspace<CR>", opts)
+    end,
+  },
+  {
+    "MagicDuck/grug-far.nvim",
+    cmd = "GrugFar",
+    opts = {},
+    keys = {
+      {
+        "<leader>sr",
+        function()
+          local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+          require("grug-far").open({
+            prefills = {
+              filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+            },
+          })
+        end,
+        mode = { "n", "v" },
+        desc = "Search and Replace",
+      },
+    },
+  },
+  {
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    opts = {},
+    keys = {
+      { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+      { "<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+      { "<leader>cs", "<cmd>Trouble symbols toggle<cr>", desc = "Symbols (Trouble)" },
+      { "<leader>cl", "<cmd>Trouble lsp toggle<cr>", desc = "LSP (Trouble)" },
+      { "<leader>xL", "<cmd>Trouble loclist toggle<cr>", desc = "Location List (Trouble)" },
+      { "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix List (Trouble)" },
+    },
+  },
+  {
+    "folke/todo-comments.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    -- stylua: ignore
+    keys = {
+      { "]t", function() require("todo-comments").jump_next() end, desc = "Next Todo Comment" },
+      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous Todo Comment" },
+      { "<leader>xt", "<cmd>Trouble todo toggle<cr>", desc = "Todo (Trouble)" },
+      { "<leader>xT", "<cmd>Trouble todo toggle filter = {tag = {TODO,FIX,FIXME}}<cr>", desc = "Todo/Fix/Fixme (Trouble)" },
+      { "<leader>st", function() require("fzf-lua").grep({ search = "TODO|HACK|FIXME|NOTE", no_esc = true }) end, desc = "Todo (FzfLua)" },
+    },
+  },
+}
