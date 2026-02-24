def5f64caacf3852e8a8d3e0a350a81bbcc9fa0f
nurazon59 <nurazon59@users.noreply.github.com>
2026-02-25 00:25:47 +0900

    feat(nvim): add treesitter-testobjects

--- config/.config/nvim/lua/plugins/treesitter.lua
+++ config/.config/nvim/lua/plugins/treesitter.lua
@@ -64,4 +64,63 @@ return {
       })
     end,
   },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    event = "BufReadPost",
+    config = function()
+      require("nvim-treesitter-textobjects").setup({
+        select = { lookahead = true },
+        move = { set_jumps = true },
+      })
+
+      local select = require("nvim-treesitter-textobjects.select")
+      local move = require("nvim-treesitter-textobjects.move")
+      local swap = require("nvim-treesitter-textobjects.swap")
+
+      for key, query in pairs({
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      }) do
+        vim.keymap.set({ "x", "o" }, key, function()
+          select.select_textobject(query)
+        end)
+      end
+
+      for key, query in pairs({
+        ["]f"] = "@function.outer",
+        ["]c"] = "@class.outer",
+        ["]a"] = "@parameter.inner",
+      }) do
+        vim.keymap.set({ "n", "x", "o" }, key, function()
+          move.goto_next_start(query)
+        end)
+      end
+
+      for key, query in pairs({
+        ["[f"] = "@function.outer",
+        ["[c"] = "@class.outer",
+        ["[a"] = "@parameter.inner",
+      }) do
+        vim.keymap.set({ "n", "x", "o" }, key, function()
+          move.goto_previous_start(query)
+        end)
+      end
+
+      vim.keymap.set("n", "<leader>na", function()
+        swap.swap_next("@parameter.inner")
+      end, { desc = "Swap next argument" })
+      vim.keymap.set("n", "<leader>pa", function()
+        swap.swap_previous("@parameter.inner")
+      end, { desc = "Swap previous argument" })
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "InsertEnter",
+    ft = { "html", "javascript", "javascriptreact", "typescript", "typescriptreact", "astro", "xml", "markdown" },
+    opts = {},
+  },
 }
