2621bc6ed18cf429d102f718733eed0ae2f9a018
nurazon59 <nurazon59@users.noreply.github.com>
2026-02-25 00:26:13 +0900

    feat(nvim): autopairs cmp

--- config/.config/nvim/lua/lsp/config.lua
+++ config/.config/nvim/lua/lsp/config.lua
@@ -124,6 +124,9 @@ return {
           { name = "buffer" },
         },
       })
+
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
     end,
   },
   {
