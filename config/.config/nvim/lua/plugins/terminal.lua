62d210d36dd178e010d9767c286d1a41adf00e55
nurazon59 <nurazon59@users.noreply.github.com>
2026-03-06 13:16:07 +0900

    fix(nvim): lazygitをtmuxに

--- config/.config/nvim/lua/plugins/terminal.lua
+++ config/.config/nvim/lua/plugins/terminal.lua
@@ -78,6 +78,7 @@ return {
       count = 3,
     })
 
+
     -- キーマップ設定関数
     local function set_terminal_keymaps()
       local opts = { noremap = true, silent = true }
@@ -93,6 +94,10 @@ return {
       end, { desc = "Toggle Vertical Terminal", unpack(opts) })
 
       vim.keymap.set("n", "<leader>tq", "<cmd>ToggleTermToggleAll<cr>", { desc = "Toggle All Terminals", unpack(opts) })
+
+      vim.keymap.set("n", "<leader>lg", function()
+        vim.fn.system("tmux display-popup -E -d " .. vim.fn.shellescape(vim.fn.getcwd()) .. " -w 70% -h 70% lazygit")
+      end, { desc = "LazyGit", unpack(opts) })
     end
 
     set_terminal_keymaps()
@@ -100,7 +105,6 @@ return {
     function _G.set_terminal_keymaps()
       local opts = { buffer = 0 }
       vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-      vim.keymap.set("t", "kj", [[<C-\><C-n>]], opts)
       vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
       vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
       vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
