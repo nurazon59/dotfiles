f93cd963c26ef0e67fe659fc284fd31839d2c60d
nurazon59 <nurazon59@users.noreply.github.com>
2026-03-06 13:16:18 +0900

    fix(nvim): remove lazygit.nvim

--- config/.config/nvim/lua/plugins/git.lua
+++ config/.config/nvim/lua/plugins/git.lua
@@ -31,23 +31,6 @@ return {
     "lewis6991/gitsigns.nvim",
   },
   {
-    "kdheepak/lazygit.nvim",
-    lazy = true,
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-    keys = {
-      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-    },
-  },
-  {
     "sindrets/diffview.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
