6c7046c12f0e409e0b68879a0ab65b5c09a57086
nurazon59 <nurazon59@users.noreply.github.com>
2026-04-01 13:35:13 +0900

    refactor(nix): system.nixに分離

--- config/nix/nix-darwin/flake.nix
+++ config/nix/nix-darwin/flake.nix
@@ -13,17 +13,17 @@
     let
       user = "itsuki54";
     in {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
+      imports = [
+        ./system.nix
+      ];
+
       environment.systemPackages =
         [ pkgs.vim
         ];
 
-      # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
       nix.settings.auto-optimise-store = true;
 
-      # Enable alternative shell support in nix-darwin.
       programs.fish.enable = true;
       system.primaryUser = user;
 
@@ -38,68 +38,8 @@
         options = "--delete-older-than 30d";
       };
 
-      system.defaults = {
-        dock = {
-          autohide = true;
-          autohide-delay = 0.2;
-          autohide-time-modifier = 0.8;
-          orientation = "left";
-          tilesize = 42;
-          launchanim = false;
-        };
-
-        finder = {
-          AppleShowAllFiles = true;
-          AppleShowAllExtensions = true;
-          ShowPathbar = true;
-          ShowStatusBar = true;
-          FXPreferredViewStyle = "Nlsv";
-          FXDefaultSearchScope = "SCcf";
-        };
-
-        NSGlobalDomain = {
-          AppleInterfaceStyle = "Dark";
-          AppleShowAllExtensions = true;
-          InitialKeyRepeat = 15;
-          KeyRepeat = 2;
-          "com.apple.trackpad.forceClick" = false;
-          "com.apple.trackpad.scaling" = 2.5;
-          _HIHideMenuBar = true;
-        };
-
-        trackpad = {
-          Clicking = true;
-          TrackpadThreeFingerDrag = true;
-        };
-
-        screensaver = {
-          askForPassword = true;
-        };
-
-        screencapture = {
-          location = "/Users/${user}/Downloads/Screenshots";
-          disable-shadow = true;
-        };
-
-        controlcenter = {
-          AirDrop = false;
-          Bluetooth = false;
-          BatteryShowPercentage = false;
-          Display = false;
-          FocusModes = false;
-          NowPlaying = false;
-          Sound = false;
-        };
-      };
-
-      # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
       system.stateVersion = 6;
-
-      # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
     };
   in
---
+++ config/nix/nix-darwin/system.nix
@@ -0,0 +1,58 @@
+{ ... }:
+let
+  user = "koshiishi";
+in {
+  system.defaults = {
+    dock = {
+      autohide = true;
+      autohide-delay = 0.2;
+      autohide-time-modifier = 0.8;
+      orientation = "left";
+      tilesize = 42;
+      launchanim = false;
+    };
+
+    finder = {
+      AppleShowAllFiles = true;
+      AppleShowAllExtensions = true;
+      ShowPathbar = true;
+      ShowStatusBar = true;
+      FXPreferredViewStyle = "Nlsv";
+      FXDefaultSearchScope = "SCcf";
+    };
+
+    NSGlobalDomain = {
+      AppleInterfaceStyle = "Dark";
+      AppleShowAllExtensions = true;
+      InitialKeyRepeat = 15;
+      KeyRepeat = 2;
+      "com.apple.trackpad.forceClick" = false;
+      "com.apple.trackpad.scaling" = 2.5;
+      _HIHideMenuBar = true;
+    };
+
+    trackpad = {
+      Clicking = true;
+      TrackpadThreeFingerDrag = true;
+    };
+
+    screensaver = {
+      askForPassword = true;
+    };
+
+    screencapture = {
+      location = "/Users/${user}/Downloads/Screenshots";
+      disable-shadow = true;
+    };
+
+    controlcenter = {
+      AirDrop = false;
+      Bluetooth = false;
+      BatteryShowPercentage = false;
+      Display = false;
+      FocusModes = false;
+      NowPlaying = false;
+      Sound = false;
+    };
+  };
+}
