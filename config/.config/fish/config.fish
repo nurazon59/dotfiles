d5aa7ac0ab117ffb10c107eee859ac0efc056c9f
nurazon59 <nurazon59@users.noreply.github.com>
2026-04-02 13:32:21 +0900

    feat(fish): add nix path

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -5,6 +5,7 @@
 # -----------------------------------------------------------------------------
 # PATH Configuration
 # -----------------------------------------------------------------------------
+fish_add_path /run/current-system/sw/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.local/share/mise/installs/tree-sitter/latest
 
