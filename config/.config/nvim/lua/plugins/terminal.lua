be9e9321121374edcb9794b03d24d7676d5e9e8d
nurazon59 <nurazon59@users.noreply.github.com>
2026-04-03 18:15:48 +0900

    fix(nvim): remove esc keymap in toggleterm

--- config/.config/nvim/lua/plugins/terminal.lua
+++ config/.config/nvim/lua/plugins/terminal.lua
@@ -43,12 +43,7 @@ return {
       -- ターミナルごとのキーマップ
       on_create = function(term)
         local opts = { buffer = term.bufnr, noremap = true, silent = true }
-        -- ターミナル内でのEsc設定
         vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], opts)
-        vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-        vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-        vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-        vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
       end,
     })
 
@@ -105,10 +100,6 @@ return {
     function _G.set_terminal_keymaps()
       local opts = { buffer = 0 }
       vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-      vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-      vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-      vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-      vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
       vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
     end
 
