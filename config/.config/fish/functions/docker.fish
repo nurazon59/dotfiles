c6d8ba1ad9d30eb4bc756f835ed5b8c4146d62fe
nurazon59 <nurazon59@users.noreply.github.com>
2026-04-07 10:37:07 +0900

    refactor(fish): chore

--- config/.config/fish/functions/docker.fish
+++
@@ -1,5 +0,0 @@
-function docker --wraps=docker --description 'Docker CLI with lazy completion loading'
-    functions --erase docker
-    docker completion fish | source
-    command docker $argv
-end
