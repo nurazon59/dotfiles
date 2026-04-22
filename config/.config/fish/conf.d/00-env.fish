716858a900a77b3905136b46c230112ce2385e83
nurazon59 <nurazon59@users.noreply.github.com>
2026-04-23 01:43:55 +0900

    fix(fish): rg path

--- config/.config/fish/conf.d/00-env.fish
+++ config/.config/fish/conf.d/00-env.fish
@@ -7,6 +7,6 @@ set -x FZF_DEFAULT_OPTS '--height 40% --reverse --cycle --bind tab:down,btab:up'
 set -x FZF_COMPLETION_OPTS '--select-1 --exit-0 --preview="bat -n --color=always {1} 2>/dev/null || lsd --color=always -a {1} 2>/dev/null" --preview-window=right:50%:wrap:border-left'
 set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
 set -x CLAUDE_CONFIG_DIR "$XDG_CONFIG_HOME/claude"
-set -gx RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/config"
+set -gx RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/ripgreprc"
 set -gx CODEX_HOME "$HOME/.config/codex"
 set -g fish_history_size 10000
