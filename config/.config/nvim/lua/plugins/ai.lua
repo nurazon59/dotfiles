8d8c8b9b0d06517b389e3221d82ce535372129c5
nurazon59 <nurazon59@users.noreply.github.com>
2026-05-08 10:13:51 +0900

    chore(nvim): remove copilot

--- config/.config/nvim/lua/plugins/ai.lua
+++ config/.config/nvim/lua/plugins/ai.lua
@@ -72,19 +72,4 @@ return {
       },
     },
   },
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    opts = {
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        keymap = {
-          accept = "<C-l>",
-        },
-      },
-      panel = { enabled = false },
-    },
-  },
 }
