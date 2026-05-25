fa2d418810fee60c7aa326a5e921288f22c4d426
nurazon59 <nurazon59@users.noreply.github.com>
2026-05-25 13:06:47 +0900

    chore(fish): abbr review me

--- config/.config/fish/conf.d/20-abbr.fish
+++ config/.config/fish/conf.d/20-abbr.fish
@@ -10,6 +10,7 @@ if status --is-interactive
     abbr --add gst 'git status'
     abbr --add gsta 'git stash'
     abbr --add lg 'lazygit'
+    abbr --add review-me "gh search prs --review-requested=@me --state=open --json title,url,author,repository --template '{{range .}}{{tablerow .repository.nameWithOwner .author.login .title .url}}{{end}}'"
     abbr --add --position anywhere nvim-conf '~/src/github.com/nurazon59/dotfiles/config/.config/nvim'
     abbr --add --position anywhere nix-conf '~/src/github.com/nurazon59/dotfiles/config/nix'
     abbr --add --position anywhere fish-conf '~/src/github.com/nurazon59/dotfiles/config/.config/fish'
