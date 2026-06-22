01f6fabb14cad024d9c8fb1a201bc23ecd08ca9e
nurazon59 <nurazon59@users.noreply.github.com>
2026-06-22 12:29:39 +0900

    chore(fish): remove git-wt integrate

--- config/.config/fish/functions/ghq_fzf.fish
+++ config/.config/fish/functions/ghq_fzf.fish
@@ -10,19 +10,6 @@ function ghq_fzf
         set -a entries $p\t$session_name\t$session_name\t$window_name
     end
 
-    # worktrees (スラッシュ入りブランチにも対応)
-    for repo in (ghq list -p)
-        test -d "$repo/.wt"; or continue
-        set -l repo_name (string replace "$ghq_root/" "" $repo)
-        set -l session_name (string replace -r '^github\.com/' '' -- $repo_name)
-        for wt_line in (git -C $repo worktree list --porcelain 2>/dev/null | string match 'worktree *')
-            set -l wt_path (string replace 'worktree ' '' $wt_line)
-            # メインworktreeはghq listで既に含まれるのでスキップ
-            test "$wt_path" = "$repo"; and continue
-            set -l rel (string replace "$repo/.wt/" "" $wt_path)
-            set -a entries $wt_path\t$session_name:$rel\t$session_name\t$rel
-        end
-    end
 
     set -l selected_line (printf '%s\n' $entries | fzf --height 40% --reverse --delimiter=\t --with-nth=2)
     if test -n "$selected_line"
